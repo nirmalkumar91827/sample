24323444a95179fdd30cf5df7f666075e5260962
nirmalkumar91827 <nirmalkumar91827@gmail.com>
2017-07-26 12:55:58 +0530

    Add files via upload

---
+++ textfile save/textfile save/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace textfile_save
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnsave = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnread = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(161, 96);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 1;
+            this.btnsave.Text = "Create";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // btnread
+            // 
+            this.btnread.Location = new System.Drawing.Point(242, 96);
+            this.btnread.Name = "btnread";
+            this.btnread.Size = new System.Drawing.Size(75, 23);
+            this.btnread.TabIndex = 5;
+            this.btnread.Text = "Read";
+            this.btnread.UseVisualStyleBackColor = true;
+            this.btnread.Click += new System.EventHandler(this.btnread_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 180);
+            this.Controls.Add(this.btnread);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnsave);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnread;
+    }
+}
+
---
+++ textfile save/textfile save/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace textfile_save
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=ESTSERVER\\SQL2012;Initial Catalog=Interview;User ID=sa;password=est$123");
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();          
+        }
+        String folderPath = @"F:\New folder\sample1.txt";
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            var value = textBox1.Text;           
+            FileStream fs = null;
+            if (!File.Exists(folderPath))
+            {
+                fs = File.Create(folderPath);
+                fs.Close();
+            }
+            fs = new FileStream(folderPath, FileMode.OpenOrCreate);
+            StreamWriter str = new StreamWriter(fs);
+            str.BaseStream.Seek(0, SeekOrigin.End);
+            str.WriteLine(value);
+            str.Flush();
+            str.Close();
+            fs.Close();
+            textBox1.Text = "";
+            
+        }
+        private void btnread_Click_1(object sender, EventArgs e)
+        {
+            if (File.Exists(folderPath))
+            {
+                using (StreamReader tr = new StreamReader(folderPath))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(tr.ReadToEnd());
+                    richTextBox1.Text = sb.ToString();
+                }
+            }
+        }
+    }
+}
+
