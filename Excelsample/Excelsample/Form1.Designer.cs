dd8dad72700e64e8e00b9311d8ea765fa21bf32a
nirmalkumar91827 <nirmalkumar91827@gmail.com>
2017-07-26 12:54:09 +0530

    Add files via upload

---
+++ Excelsample/Excelsample/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Excelsample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtid1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtmobile = new System.Windows.Forms.TextBox();
+            this.txtaddress = new System.Windows.Forms.TextBox();
+            this.txtage = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnexport = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtid1
+            // 
+            this.txtid1.AutoSize = true;
+            this.txtid1.Location = new System.Drawing.Point(110, 71);
+            this.txtid1.Name = "txtid1";
+            this.txtid1.Size = new System.Drawing.Size(16, 13);
+            this.txtid1.TabIndex = 0;
+            this.txtid1.Text = "Id";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(71, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Mobile No";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Address";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(174, 71);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 20);
+            this.txtid.TabIndex = 5;
+            // 
+            // txtmobile
+            // 
+            this.txtmobile.Location = new System.Drawing.Point(174, 179);
+            this.txtmobile.Name = "txtmobile";
+            this.txtmobile.Size = new System.Drawing.Size(100, 20);
+            this.txtmobile.TabIndex = 6;
+            // 
+            // txtaddress
+            // 
+            this.txtaddress.Location = new System.Drawing.Point(174, 210);
+            this.txtaddress.Name = "txtaddress";
+            this.txtaddress.Size = new System.Drawing.Size(100, 20);
+            this.txtaddress.TabIndex = 7;
+            // 
+            // txtage
+            // 
+            this.txtage.Location = new System.Drawing.Point(174, 143);
+            this.txtage.Name = "txtage";
+            this.txtage.Size = new System.Drawing.Size(100, 20);
+            this.txtage.TabIndex = 8;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(174, 107);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 9;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(186, 259);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 10;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(314, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(432, 159);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btnexport
+            // 
+            this.btnexport.Location = new System.Drawing.Point(499, 259);
+            this.btnexport.Name = "btnexport";
+            this.btnexport.Size = new System.Drawing.Size(75, 23);
+            this.btnexport.TabIndex = 12;
+            this.btnexport.Text = "Export";
+            this.btnexport.UseVisualStyleBackColor = true;
+            this.btnexport.Click += new System.EventHandler(this.btnexport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 349);
+            this.Controls.Add(this.btnexport);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtage);
+            this.Controls.Add(this.txtaddress);
+            this.Controls.Add(this.txtmobile);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtid1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtid1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtmobile;
+        private System.Windows.Forms.TextBox txtaddress;
+        private System.Windows.Forms.TextBox txtage;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnexport;
+    }
+}
+
---
+++ Excelsample/Excelsample/Form1.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excelsample
+{  
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=ESTSERVER\\SQL2012;Initial Catalog=Interview;User ID=sa;password=est$123");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        Microsoft.Office.Interop.Excel.Application excel;
+        Microsoft.Office.Interop.Excel.Workbook excelworkBook;
+        Microsoft.Office.Interop.Excel.Worksheet excelSheet;
+        Microsoft.Office.Interop.Excel.Range excelCellrange;
+        Microsoft.Office.Interop.Excel.Sheets xlBigSheet;
+        private static Microsoft.Office.Interop.Excel.Worksheet mWSheet1;
+        String folderPath = @"F:\New folder\sample1.xlsx";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void DisplayData()
+        {
+            con.Open();
+            System.Data.DataTable dt = new System.Data.DataTable();
+            adapt = new SqlDataAdapter("select * from customers", con);
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            int i;
+            int ColumnIndex = 0;
+            System.Data.DataTable dt = new System.Data.DataTable();
+            if (!File.Exists(folderPath))
+            {
+                FileStream fileStream = new FileStream(folderPath, FileMode.Create);
+                fileStream.Close();
+                excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+                dt.Columns.Add("Id");
+                dt.Columns.Add("Name");
+                dt.Columns.Add("Age");
+                dt.Columns.Add("Mobile No");
+                dt.Columns.Add("Address");
+                dt.Rows.Add(txtid.Text, txtname.Text, txtage.Text, txtmobile.Text, txtaddress.Text);
+
+                excel.Application.Workbooks.Add(Type.Missing);
+
+
+                foreach (DataColumn col in dt.Columns)
+                {
+                    ColumnIndex++;
+                    excel.Cells[1, ColumnIndex] = col.ColumnName;
+                }
+
+                for (i = 0; i <= dt.Rows.Count - 1; i++)
+                {
+                    DataRow row = dt.Rows[i];
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        excel.Cells[i + 2, j + 1] = row.ItemArray[j].ToString();
+                    }
+                }
+                excel.ActiveWorkbook.SaveCopyAs(folderPath);
+                excel.ActiveWorkbook.Saved = true;
+                excel.Quit();
+                txtid.Text = "";
+                txtname.Text = "";
+                txtage.Text = "";
+                txtmobile.Text = "";
+                txtaddress.Text = "";
+            }
+            else
+            {
+                excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+                //  excelworkBook = excel.Workbooks.Open(folderPath, 0, false, 5, "", "", false, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+                dt = new System.Data.DataTable();
+
+                dt.Columns.Add("Id");
+                dt.Columns.Add("Name");
+                dt.Columns.Add("Age");
+                dt.Columns.Add("Mobile No");
+                dt.Columns.Add("Address");
+                dt.Rows.Add(txtid.Text, txtname.Text, txtage.Text, txtmobile.Text, txtaddress.Text);
+
+                string filepath = folderPath;
+
+                excel.Application.Workbooks.Add(Type.Missing);
+                foreach (DataColumn col in dt.Columns)
+                {
+                    ColumnIndex++;
+                    excel.Cells[1, ColumnIndex] = col.ColumnName;
+                }
+                excelworkBook = excel.Workbooks.Open(folderPath, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                xlBigSheet = excelworkBook.Worksheets;
+                mWSheet1 = (Microsoft.Office.Interop.Excel.Worksheet)xlBigSheet.get_Item("Sheet1");
+                Microsoft.Office.Interop.Excel.Range range = mWSheet1.UsedRange;
+                int colCount = range.Columns.Count;
+                int rowCount = range.Rows.Count;
+
+                for (i = 0; i <= dt.Rows.Count - 1; i++)
+                {
+                    DataRow row = dt.Rows[i];
+                    for (int j = 0; j < dt.Columns.Count; j++)
+                    {
+                        excel.Cells[rowCount + 1, j + 1] = row.ItemArray[j].ToString();
+                    }
+                }
+                excel.ActiveWorkbook.SaveCopyAs(folderPath);
+                excel.ActiveWorkbook.Saved = true;
+                excel.Quit();
+                txtid.Text = "";
+                txtname.Text = "";
+                txtage.Text = "";
+                txtmobile.Text = "";
+                txtaddress.Text = "";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+
+        private void btnexport_Click(object sender, EventArgs e)
+        {
+            int i;
+            int ColumnIndex = 0;
+            if (!File.Exists(folderPath))
+            {
+                FileStream fileStream = new FileStream(folderPath, FileMode.Create);
+                fileStream.Close();
+                excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+                
+                excel.Application.Workbooks.Add(Type.Missing);
+
+
+                for (int k = 1; k < dataGridView1.Columns.Count + 1; k++)
+                {
+                    excel.Cells[1, k] = dataGridView1.Columns[k - 1].HeaderText;
+                }
+
+
+
+                // storing Each row and column value to excel sheet
+                for (int l = 0; l < dataGridView1.Rows.Count - 1; l++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        excel.Cells[l + 2, j + 1] = dataGridView1.Rows[l].Cells[j].Value.ToString();
+                    }
+                }
+                excel.ActiveWorkbook.SaveCopyAs(folderPath);
+                excel.ActiveWorkbook.Saved = true;
+                excel.Quit();
+            }
+            else
+            {
+                excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+                string filepath = folderPath;
+
+                excel.Application.Workbooks.Add(Type.Missing);
+                // storing Each row and column value to excel sheet
+                for (int l = 0; l < dataGridView1.Rows.Count - 1; l++)
+                {
+                    excelworkBook = excel.Workbooks.Open(folderPath, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                    xlBigSheet = excelworkBook.Worksheets;
+                    mWSheet1 = (Microsoft.Office.Interop.Excel.Worksheet)xlBigSheet.get_Item("Sheet1");
+                    Microsoft.Office.Interop.Excel.Range range = mWSheet1.UsedRange;
+                    int colCount = range.Columns.Count;
+                    int rowCount = range.Rows.Count;
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {                       
+                        excel.Cells[rowCount + 1, j + 1] = dataGridView1.Rows[l].Cells[j].Value.ToString();
+                    }
+                    excel.ActiveWorkbook.SaveCopyAs(folderPath);
+                    excel.ActiveWorkbook.Saved = true;
+                    excel.Quit();
+                }
+            }
+            
+           
+        }
+    }
+}
